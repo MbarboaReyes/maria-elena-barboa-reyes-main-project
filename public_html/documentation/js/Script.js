36b489f26cb8f251a44e22b67f2b21f8212bd6cf
MariaElena Barboa-Reyes <Mbarboareyes@cnm.edu>
2022-02-14 20:14:36 -0700

    mailgun email

--- public_html/documentation/js/Script.js
+++ public_html/documentation/js/Script.js
@@ -1,53 +1,53 @@
 $(document).ready(function(){
-        $('#contact').Validate({
-            debug: true,
-            errorClass: 'alert alert-danger'
-            ErrorLabelContainer: '#output-area',
-            errorElement: 'div',
-            //rules here define what is good or bad input
-            //each rule starts with the form input element's NAME attribute
-            rules: {
-                name: {
-                    required:{
-
-                    }
-                    email {
-                        email: true,
-                        required: true
-                    },
-                    message: {
-                        required: true
-                        maxlength: 2000
-                    }
-                }
-                messages:{
-                    name: {
-                        required: 'Name is required.'
-                    }
-                    email:{
-                        email: 'Please provide a valid email.',
-                        required: 'Email is required.'
-                    },
-                    Message: {
-                        required: 'A Message is required.',
-                        maxlength: 'Message is too long.'
-                    }
-                }
-                submitHandler: (form) => {
-                    $('#contact').ajaxSubmit({
-                        type: 'POST',
-                        url: $('#contact').attr('action'),
-                        success: (ajaxOutput) => {
-                            $('#output-area').css('display','')
-                            $('#output-area').html(ajaxOutput)
-
-                            if($('.alert-success'>=1)){
-                                $('#contact')[0].reset()
-                            }
-                        }
-                    })
-                }
+    $('#contact').validate({
+        debug: true,
+        errorClass: 'alert alert-danger',
+        ErrorLabelContainer: '#output-area',
+        errorElement: 'div',
+        // rules here define what is good or bad input
+        //each rule starts with the form input element's NAME attribute
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                email: true,
+                required: true
+            },
+            message:{
+                required: true,
+                maxlength: 2000
             }
+        },
+        messages:{
+            name: {
+                required: 'Name is required.'
+            },
+            email: {
+                email: 'Please provide a valid email.',
+                required: 'Email is required.'
+            },
+            message: {
+                required: 'A Message is required.',
+                maxlength: 'Message is too long.'
+            }
+        },
+        submitHandler: (form) => {
+            $('#contact').ajaxSubmit({
+                type: 'POST',
+                url: $('#contact').attr('action'),
+                success: (ajaxOutput) => {
+                    $('#output-area').css('display','')
+                    $('#output-area').html(ajaxOutput)
 
-        })
+                    if($('.alert-success') >= 1){
+                        $('#contact')[0].reset()
+                    }
+
+                }
+            })
+
+
+        }
+    })
 })
---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import {Application, Router} from 'express'
+import {Request, Response} from 'express'
+
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const Recaptcha = require('express-recaptcha')
+const formData = require('form-data')
+const Mailgun = require('mailgun.js')
+const mailgun = new Mailgun(formData)
+
+const {check, validationResult} = require('express-validator')
+
+const validation = [
+    check('name', 'a valid name is required.').not().isEmpty().trim().escape(),
+    check('email', 'please provid a valid email.').isEmail(),
+    check('subject').optional().trim().escape(),
+    check('message', 'A message of 2000 characters or less is required.').trim().escape().isLength({min: 1, max: 2000})
+]
+const app = express()
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SITE_KEY)
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY})
+const handleGetRequest = (request: Request, response: Response)=>{
+    return response.json('this thing is on!')
+}
+
+const handlePostRequest = (request: Request, response: Response) => {
+
+    console.log(request.body)
+
+    response.append('Content-Type', 'text/html')
+// @ts-ignore
+// 	if(request.recaptcha.error){
+// 		return response.send(
+// 			`<div class='alert alert-danger' role='alert'><strong>Oh Snap!</strong>There was a Recaptcha error. Please try again.</div>`
+// 		)
+// 	}
+
+    const errors = validationResult(request)
+
+    if(errors.isEmpty() === false){
+        const currentError = errors.array()[0]
+        return response.send(
+            `<div class='alert alert-danger' role='alert'><strong>Oh Snap!</strong>${currentError.msg}</div>`
+        )
+    }
+
+    // const name = request.body.name
+    // const email = request.body.email
+    // const subject = request.body.subject
+    // const message = request.body.message
+    // The above assignments can be done using the single line below.
+    const {name, email, subject, message} = request.body
+
+    const mailgunData = {
+        to: process.env.MAIL_RECIPIENT,
+        from: `${name} <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+        subject: `${email}: ${subject}`,
+        text: message
+    }
+
+    mg.messages.create(process.env.MAILGUN_DOMAIN, mailgunData)
+        .then((msg: any) =>
+            response.send(`<div class='alert alert-success' role='alert'>Email Successfully Sent</div>`)
+        )
+        .catch((error: any) =>
+            response.send(`<div class='alert alert-danger' role='alert'>Email Failed. Please try again.</div>`)
+        )
+
+
+}
+
+const indexRoute = express.Router()
+
+indexRoute.route('/')
+    .get(handleGetRequest)
+    .post(recaptcha.middleware.verify, validation, handlePostRequest)
+
+app.use('/apis', indexRoute)
+
+app.listen(4200, () =>{
+    console.log('Express Successfully Built')
+})
